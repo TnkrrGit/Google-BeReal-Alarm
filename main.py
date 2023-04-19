eb1150df8b9df3c7a7db9376c7ca36538833fa05
Jorick Bouw <jorick.bouw@student.aventus.nl>
2023-04-20 00:28:44 +0200

    Chromecast Casting working. YML Config commingsoon

--- main.py
+++ main.py
@@ -9,7 +9,35 @@ import schedule
 import time
 from datetime import datetime
 
+### Imports Google Home ###
+
+import pychromecast
+import time
+from cryptography.hazmat.bindings.openssl.binding import Binding
+
+### Startup ###
+
+### Google Home Variables ###
+
+chromecast_name = "all"
+chromecasts = pychromecast.get_chromecasts()
+print(chromecasts)
+cast = next(
+    cc for cc in chromecasts if cc.device.friendly_name == chromecast_name)
+print(cast)
+mc = cast.media_controller
+
+### Google Home Functions ###
+
+
+def alarm():
+    mc.play_media("https://dx35vtwkllhj9.cloudfront.net/universalstudios/super-mario-bros-plumbing/images/soundbites/completion-audio.mp3",
+                  content_type="audio/mp3")
+    mc.block_until_active()
+    mc.play()
+
 ### BeReal Variables ###
+
 
 # Replace with your API key found on https://bereal.devin.fun/
 # Replace with your desired region found on https://bereal.devin.fun/
@@ -37,6 +65,7 @@ def trigger_alarm():
     global BeRealShot
     if BeRealShot == False:
         print("Alarm triggered!")
+        alarm()
         BeRealShot = True
 
 
